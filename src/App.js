789b61d9eec2dd055512d29e3546b5acf8e115e5
STEPH SAKAI <stephaniesakai@stephanies-mbp.attlocal.net>
2021-04-11 08:57:51 -0700

    React Rolodex Exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      label: "ANNOYING PERSON ELIMINATOR",
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  handleChange = e => {
+    this.setState({ searchField: e.target.value }); 
+  } // when Component App is getting run by JS for the first time, it instantiates the new App Class and sees this handleChange method that points to an arrow function. So then it defines the arrow function and the moment it sees 'this' and because it's an arrow function it automatically binds 'this' to the place where this arrow function was defined in the first place. The context of this arrow function is our App Component.
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }));
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+      return (
+        <div className="App">
+          <SearchBox
+            placeholder='Search Monsters'
+            handleChange = {this.handleChange}
+           />
+          <CardList monsters={filteredMonsters} />
+          <p>{this.state.label}</p>
+          <button onClick={() => this.setState({ label: "REMOVED" })}>
+            DELETE
+          </button>
+        </div>
+      )
+    }
 }
 
 export default App;
